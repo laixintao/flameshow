2889b8b473bccdf96f26df117269676a2f01cc90
laixintao <laixintaoo@gmail.com>
2023-09-27 17:55:28 +0800

    for i686

--- scripts/prepare_golang.sh
+++ scripts/prepare_golang.sh
@@ -10,6 +10,8 @@ if [ $ARCH == "x86_64" ]; then
   go_arch=amd64
 elif [ $ARCH == "arm64" ]; then
   go_arch=arm64
+elif [ $ARCH == "i686" ]; then
+  go_arch=386
 else
   echo "unknown arch=${ARCH}"
   exit 1
