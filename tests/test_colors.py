0c9a7b1e6ee35d2ee71f17792da05fcbd1eb9edc
laixintao <laixintaoo@gmail.com>
2023-10-04 23:43:34 +0800

    add tests for colors

---
+++ tests/test_colors.py
@@ -0,0 +1,26 @@
+from flameshow.colors import LinaerColorPlatte, flamegraph_random_color_platte
+from textual.color import Color
+
+
+def test_linaer_color_platte():
+    platte = LinaerColorPlatte()
+
+    color1 = platte.get_color("1")
+    color3 = platte.get_color("1")
+
+    color2 = platte.get_color("2")
+    assert color1 is color3
+
+    assert isinstance(color2, Color)
+
+
+def test_flamegraph_random_color_platte():
+    platte = flamegraph_random_color_platte
+
+    color1 = platte.get_color("1")
+    color3 = platte.get_color("1")
+
+    color2 = platte.get_color("2")
+    assert color1 is color3
+
+    assert isinstance(color2, Color)
