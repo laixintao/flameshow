9e9a97f4fcb7c611242ebb93b73b657b0b53ddf1
laixintao <laixintaoo@gmail.com>
2024-01-08 15:16:00 +0800

    fix testcase caused by `render_one_frame_detail` return type changing.

--- tests/test_stackcollapse_parse/test_model.py
+++ tests/test_stackcollapse_parse/test_model.py
@@ -6,4 +6,6 @@ def test_frame_model_render():
         "java::abc", 1, parent=None, children=[], values=[13]
     )
     render_result = f.render_one_frame_detail(f, 0, "count")
-    assert render_result == ["java::abc\n"]
+    assert len(render_result) == 1
+    item = render_result[0]
+    assert item.markup == "java::abc\n"
